8795ca177deeb303e298703a06d9212c695edae2
Sam S <Saprykinse@gmail.com>
2021-07-08 17:03:41 +0300

    dest token

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -25,7 +25,7 @@ CLONE_DIR=$(mktemp -d)
 echo "Cloning destination git repository"
 git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
-git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://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@github.com/SamSaprykin/dest-actions.git" "$CLONE_DIR"
+git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://$GB_TOKEN@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
 if [ ! -z "$INPUT_DESTINATION_BRANCH_CREATE" ]
 then
